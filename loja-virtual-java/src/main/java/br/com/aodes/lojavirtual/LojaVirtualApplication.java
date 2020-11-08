a7148df157e9966839a45cb53f5cc5d19d232b91
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-08 19:25:57 -0300

    Criando operação de instanciação

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/LojaVirtualApplication.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/LojaVirtualApplication.java
@@ -1,13 +1,34 @@
 package br.com.aodes.lojavirtual;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojaVirtualApplication {
+import br.com.aodes.lojavirtual.domain.Categoria;
+import br.com.aodes.lojavirtual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class LojaVirtualApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
