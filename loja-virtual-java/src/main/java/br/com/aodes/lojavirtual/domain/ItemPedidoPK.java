dd9ac6acb97fefa4ae9526b546986e98880b7583
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-09 16:46:50 -0300

    Endpoint /pedidos/{id} disponibilizado

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/domain/ItemPedidoPK.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/domain/ItemPedidoPK.java
@@ -6,6 +6,7 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+
 @Embeddable // anotação de subtipo
 public class ItemPedidoPK implements Serializable {
 	private static final long serialVersionUID = 1L;
