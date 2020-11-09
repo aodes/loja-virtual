f370edbb0d06b764ba05581d19ff99fc7b1fa800
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-09 00:43:48 -0300

    Ajustes no endpoint /categorias{id}

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/CategoriaService.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/CategoriaService.java
@@ -1,10 +1,13 @@
 package br.com.aodes.lojavirtual.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.aodes.lojavirtual.domain.Categoria;
 import br.com.aodes.lojavirtual.repositories.CategoriaRepository;
+import br.com.aodes.lojavirtual.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -13,7 +16,12 @@ public class CategoriaService {
 	private CategoriaRepository categoriaRepository;
 	
 	public Categoria findById(Integer id) {
-		return categoriaRepository.findById(id).get();
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+			
+		if(obj.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
+		return obj.get();
 	}
 	
 }
