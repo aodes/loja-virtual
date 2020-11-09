0acf2836a6db671f78f62a3bc413030d18b1efb9
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-08 23:56:05 -0300

    Produto e associação muitos para muitos

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/domain/Produto.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/domain/Produto.java
@@ -13,24 +13,28 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double valor;
 
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 
-	public Categoria() {
+	public Produto() {
+
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double valor) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.valor = valor;
 	}
 
 	public Integer getId() {
@@ -49,12 +53,20 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public List<Categoria> getCategorias() {
+		return categorias;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
 	}
 
 	@Override
@@ -73,7 +85,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
