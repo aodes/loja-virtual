bea11f7ff7c0eef4dbb04a9936a103398b38efa3
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-09 01:28:59 -0300

    Estado e Cidade

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/LojaVirtualApplication.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/LojaVirtualApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.aodes.lojavirtual.domain.Categoria;
+import br.com.aodes.lojavirtual.domain.Cidade;
+import br.com.aodes.lojavirtual.domain.Estado;
 import br.com.aodes.lojavirtual.domain.Produto;
 import br.com.aodes.lojavirtual.repositories.CategoriaRepository;
+import br.com.aodes.lojavirtual.repositories.CidadeRepository;
+import br.com.aodes.lojavirtual.repositories.EstadoRepository;
 import br.com.aodes.lojavirtual.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class LojaVirtualApplication implements CommandLineRunner{
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
@@ -44,6 +54,21 @@ public class LojaVirtualApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
