5b2291612bfec2bb64fe542ee089674a4fa3caf8
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-10 11:58:42 -0300

    PUT, DELETE e GET para Cliente

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/ClienteService.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.aodes.lojavirtual.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.aodes.lojavirtual.domain.Cliente;
+import br.com.aodes.lojavirtual.dto.ClienteDTO;
 import br.com.aodes.lojavirtual.repositories.ClienteRepository;
+import br.com.aodes.lojavirtual.services.exceptions.DataIntegrityException;
 import br.com.aodes.lojavirtual.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,4 +29,40 @@ public class ClienteService {
 		}
 		return obj.get();
 	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj , obj);
+		return clienteRepository.save(newObj);
+	}
+
+	public void deleteById(Integer id) {
+		findById(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedido");
+		}
+
+	}
+
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		//throw new UnsupportedOperationException("Metodo ainda não implementado	");
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
 }
