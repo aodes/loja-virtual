b8b9ec6da5fdb3754efbd652f8a6de33bf1fd943
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-12 16:10:46 -0300

    Adicionando o campo senha a Cliente

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/ClienteService.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/ClienteService.java
@@ -10,6 +10,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.aodes.lojavirtual.domain.Cidade;
@@ -27,10 +28,13 @@ import br.com.aodes.lojavirtual.services.exceptions.ObjectNotFoundException;
 public class ClienteService {
 
 	@Autowired
-	ClienteRepository clienteRepository;
+	private ClienteRepository clienteRepository;
 	
 	@Autowired
-	EnderecoRepository enderecoRepository;
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = clienteRepository.findById(id);
@@ -73,14 +77,13 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		// throw new UnsupportedOperationException("Metodo ainda não implementado ");
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null,null);
 	}
 
 
 	public Cliente fromDTO(ClienteNewDTO objDto) {
 
-		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
-				TipoCliente.toEnum(objDto.getTipo()));
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),TipoCliente.toEnum(objDto.getTipo()), bCryptPasswordEncoder.encode(objDto.getSenha()) );
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
 		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
 				objDto.getBairro(), objDto.getCep(), cli, cid);
