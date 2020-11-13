6685a6113d827951bd48f3415893ab83e10f3578
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-13 18:58:17 -0300

    Enviando imagem via endpoint para o AmazonS3

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/LojaVirtualApplication.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/LojaVirtualApplication.java
@@ -1,27 +1,19 @@
 package br.com.aodes.lojavirtual;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.aodes.lojavirtual.services.S3Service;
-
 @SpringBootApplication
-public class LojaVirtualApplication implements CommandLineRunner{
+public class LojaVirtualApplication implements CommandLineRunner {
 
-@Autowired
-private S3Service s3Service;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		s3Service.uploadFile("C:\\Users\\ander\\Desktop\\img.jpg");
-	}
 
+	}
 
 }
--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/ClienteService.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/ClienteService.java
@@ -1,5 +1,6 @@
 package br.com.aodes.lojavirtual.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.aodes.lojavirtual.domain.Cidade;
 import br.com.aodes.lojavirtual.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente findById(Integer id) {
 		
@@ -113,6 +118,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
