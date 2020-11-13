f181f7597d5fa7ba350f0b39410f93cf63f1a7c6
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-13 17:51:41 -0300

    Configurando e Salvando o primeiro arquivo no AmazonS3

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/LojaVirtualApplication.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/LojaVirtualApplication.java
@@ -1,15 +1,26 @@
 package br.com.aodes.lojavirtual;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.aodes.lojavirtual.services.S3Service;
+
 @SpringBootApplication
-public class LojaVirtualApplication{
+public class LojaVirtualApplication implements CommandLineRunner{
 
-
+@Autowired
+private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		s3Service.uploadFile("C:\\Users\\ander\\Desktop\\img.jpg");
 	}
 
 
