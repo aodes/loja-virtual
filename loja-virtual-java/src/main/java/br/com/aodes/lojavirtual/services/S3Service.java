e575dd39a5f003eb7395e627bae86a97c2aa0bad
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-13 19:43:46 -0300

    Tratando as exceções adequadamente

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/S3Service.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/S3Service.java
@@ -15,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import br.com.aodes.lojavirtual.services.exceptions.FileException;
+
 @Service
 public class S3Service {
 
@@ -35,7 +37,7 @@ public class S3Service {
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
 
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 
@@ -51,7 +53,7 @@ public class S3Service {
 
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
