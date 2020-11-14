695e180fcb059609752c7f9eb102a355f251d6e2
Anderson de Oliveira <anderson.aodes@icloud.com>
2020-11-14 11:35:28 -0300

    Usando padrão de nomes para imagens

--- loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/ClienteService.java
+++ loja-virtual-java/src/main/java/br/com/aodes/lojavirtual/services/ClienteService.java
@@ -1,5 +1,6 @@
 package br.com.aodes.lojavirtual.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +8,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -32,25 +34,31 @@ import br.com.aodes.lojavirtual.services.exceptions.ObjectNotFoundException;
 @Service
 public class ClienteService {
 
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	
+
 	@Autowired
 	private S3Service s3Service;
 
+	@Autowired
+	private ImageService imageService;
+
 	public Cliente findById(Integer id) {
-		
+
 		UserSS user = UserService.authenticated();
 		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso Negado");
 		}
-		
+
 		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
@@ -91,13 +99,13 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		// throw new UnsupportedOperationException("Metodo ainda não implementado ");
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null,null);
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
-
 
 	public Cliente fromDTO(ClienteNewDTO objDto) {
 
-		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),TipoCliente.toEnum(objDto.getTipo()), bCryptPasswordEncoder.encode(objDto.getSenha()) );
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), bCryptPasswordEncoder.encode(objDto.getSenha()));
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
 		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
 				objDto.getBairro(), objDto.getCep(), cli, cid);
@@ -113,25 +121,20 @@ public class ClienteService {
 		}
 		return cli;
 	}
-	
 
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
+
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
 		UserSS user = UserService.authenticated();
 		if (user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		URI uri = s3Service.uploadFile(multipartFile);
-		
-		Cliente cli = clienteRepository.findById(user.getId()).get();
-		cli.setImageUrl(uri.toString());
-		clienteRepository.save(cli);
-		
-		return uri;
-	}
 
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
+	}
 }
